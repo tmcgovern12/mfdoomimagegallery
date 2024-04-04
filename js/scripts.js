8ff2674fab6e3b0e767cb53b2bffb0bf8660eaed
tadhg <tmcgovern@mail.endicott.edu>
2024-04-04 08:25:51 -0400

    part 1

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+   // When the user scrolls down 800px from the top of the document, show the button
+   window.onscroll = function() {scrollFunction()};
+
+   function scrollFunction() {
+     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+       document.getElementById("cornerBtn").style.display = "block";
+     } else {
+       document.getElementById("cornerBtn").style.display = "none";
+     }
+   }
+
+   // When the user clicks on the button, scroll to the top of the document
+   function topFunction() {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
+   }
+
+
+
+
+   
+
+
+
+
+
+
+
+// Javascript - Get Current Year for the Footer
+window.onload = function() {
+    let d = new Date();
+    document.getElementById("copyright").innerHTML = d.getFullYear();
+}
